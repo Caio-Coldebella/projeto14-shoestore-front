9dceaec827dad24180c5bbd7f9fb90a8db0bb3f4
Caio-Coldebella <c232621@dac.unicamp.br>
2022-09-14 20:31:43 -0300

    Criação das rotas

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import { BrowserRouter, Routes, Route} from "react-router-dom";
 import "./css/reset.css";
 import "./css/style.css";
-
+import Home from "./elements/Home";
+import Item from "./elements/Item";
+import Createaccount from "./elements/Createaccount";
+import Login from "./elements/Login";
+import Checkout from "./elements/Checkout";
 
 export default function App(){
   return(
     <>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/:idItem" element={<Item/>}/>
+        <Route path="/sign-in" element={<Createaccount/>}/>
+        <Route path="/sign-up" element={<Login/>}/>
+        <Route path="/checkout" element={<Checkout/>}/>
+      </Routes>
+    </BrowserRouter>
     </>
   );
 }
