67f290febe142e6070ff3598a619d2ea807d0c5a
LucasZuq <lucasdeozuquetto@hotmail.com>
2022-09-15 18:10:10 -0300

    requisição post para o /sign-in e para o /sign-up com um objeto contendo os dados preenchidos pelo form

--- src/elements/Login.js
+++ src/elements/Login.js
@@ -1,6 +1,42 @@
-export default function Login(){
-    return(
-        <>
-        </>
-    );
+import axios from "axios";
+import { useState } from "react";
+import Authentication from './styled-components/AuthenticationStyle';
+
+export default function Login() {
+   const [name, setName] = useState();
+   const [password, setPassword] = useState();
+   function handleForm(e) {
+      e.preventDefault();
+      const loginData = {
+         name,
+         password
+      };
+      axios
+         .post("http://localhost:5000/sign-in", loginData)
+         .then(() => console.log("post loginData feito"))
+         .catch(() => console.log("deu ruim no post user"));
+   }
+   return (
+      <Authentication>
+         <div>
+            <form onSubmit={handleForm}>
+               <input
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  type="number"
+                  placeholder="Nome"
+                  required
+               />
+               <input
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  type="password"
+                  placeholder="Senha"
+                  required
+               />
+               <button type="submit">Confirmar</button>
+            </form>
+         </div>
+      </Authentication>
+   );
 }
